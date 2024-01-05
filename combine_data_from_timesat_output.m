de1aa93aebbaf8aeda14a180d3738132cc9ec2d3
Yang Shao <yshao@n2d037b0e.cns.vt.edu>
2024-01-05 11:12:08 -0500

    combine signals from TIMESAT

---
+++ combine_data_from_timesat_output.m
@@ -0,0 +1,18 @@
+%This matlab script combines outputs (smoothed signals) from TIMESAT
+%The dataset can be fairly large so we have 21 (total_set) data segments for TIMESAT
+
+total_set=21
+for j=1:total_set
+    j
+temp=[];
+for i=1:552
+outfile1=sprintf('/media/newhd/yshao/output/sg%d/sout_%s',j,sprintf('%04d',i));
+        tfile=fopen(outfile1,'r');
+        a=fread(tfile, 'float');
+        fclose(tfile);
+        
+        temp=[temp a];
+end
+f=sprintf('sub_%d',j);
+save(f,"temp","-v7.3")
+end
