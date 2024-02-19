dc9d09cde719497860ca3ef43d77b34fb7ca9b1d
yshao <yshao@vt.edu>
2024-02-19 17:48:56 -0500

    added matlab script for stratified sampling and extract ndvi values from sample locations

---
+++ extract_ndvi_samplelocation.m
@@ -0,0 +1,17 @@
+%extract ndvi values from sample locations
+temp1=[];
+temp2=[];
+for year=2001:2023
+    for j=1:8:366
+f=sprintf('/media/newhd/yshao/evi250_8day/ndvi%d_%d',year,j)
+load(f)
+
+% Extracting values for pixels with value 1s or 2s in the first image
+s_sig = ndvi(sub2ind(size(ndvi), row1s,col1s));
+d_sig = ndvi(sub2ind(size(ndvi), row2s,col2s));
+
+temp1=[temp1 s_sig];
+temp2=[etmp2 d_sig];
+
+    end
+end
