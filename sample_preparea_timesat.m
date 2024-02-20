b8f554dd144a05fec8681e29fee4604444365072
yshao <yshao@vt.edu>
2024-02-20 15:30:02 -0500

    added matlab script - prepare data for timesat

--- sample_preparea_timesat.m
+++ sample_preparea_timesat.m
@@ -1,12 +1,11 @@
 %write to binary files - ready for timesat 
-infile=sprintf('/home/yshao/jie/stemp')
+infile=sprintf('/home/yshao/jie/stemp.mat')
 load(infile);
 for i=1:1058
-outfile1=sprintf('/home/yshao/jie/smaples/evi%d',i);
+outfile1=sprintf('/home/yshao/jie/samples/evi%d',i);
         tfile=fopen(outfile1,'w');
         temp1=sig(:,i);
         fwrite(tfile, temp1,'float');
         fclose(tfile)
         i
 end
-
