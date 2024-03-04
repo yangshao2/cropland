f26727ce3779300d3e5e8ae20fa9896ce9f8375a
yshao <yshao@vt.edu>
2024-03-03 19:50:26 -0500

    added example_of_peakdetection.m script

---
+++ peak_detection_figure.m
@@ -0,0 +1,21 @@
+% URL of the .mat file on GitHub (use the direct link from the "Raw" button)
+url = 'https://github.com/yangshao2/cropland/raw/master/mfigure.mat'; 
+% The local filename to save the .mat file
+filename = 'localfile.mat'; 
+
+% Download the file
+websave(filename, url);
+
+% Load the .mat file into MATLAB
+load(filename);
+
+dcrop=u(:,1);
+scrop=u(:,2);
+
+
+%[pk,lk,w,p] = findpeaks(dcrop(1:92),'MinPeakHeight',0.3,'MinPeakDistance',4,'Sortstr','descend');
+subplot(1,2,1)
+findpeaks(scrop(1:92),'MinPeakHeight',0.3,'MinPeakDistance',10,'Annotate','extents')
+
+subplot(1,2,2)
+findpeaks(dcrop(1:92),'MinPeakHeight',0.3,'MinPeakDistance',10,'Annotate','extents')
