573d2eead09b81a8c92d7edbf3a1a111e5c67c60
yshao <yshao@vt.edu>
2024-03-04 09:17:55 -0500

    added example_of_peakdetection.m script

--- peak_detection_figure.m
+++ peak_detection_figure.m
@@ -1,19 +1,32 @@
 % URL of the .mat file on GitHub (use the direct link from the "Raw" button)
-%url = 'https://github.com/yangshao2/cropland/raw/master/mfigure.mat'; 
+url = 'https://github.com/yangshao2/cropland/raw/master/mfigure.mat'; 
 % The local filename to save the .mat file
 filename = 'localfile.mat'; 
 
 % Download the file
 websave(filename, url);
 
+
 % Load the .mat file into MATLAB
 load(filename);
+x=1:8:365
 
 dcrop=u(:,1);
 scrop=u(:,2);
 
+
 subplot(1,2,1)
-findpeaks(scrop(1:92),'MinPeakHeight',0.35,'MinPeakDistance',10,'Annotate','extents')
+findpeaks(scrop(1:46),x,'MinPeakHeight',0.35,'MinPeakDistance',4,'Annotate','extents')
+ylim([0 1]);
+xlabel('Day of Year','FontSize', 14);
+ylabel('NDVI','FontSize', 14)
+set(gca, 'FontSize', 14);
+text(10, 0.95, '(a) Single crop', 'FontSize', 18);
+legend('off');
 
 subplot(1,2,2)
-findpeaks(dcrop(1:92),'MinPeakHeight',0.35,'MinPeakDistance',10,'Annotate','extents')
+findpeaks(dcrop(1:46),x,'MinPeakHeight',0.35,'MinPeakDistance',4,'Annotate','extents')
+xlabel('Day of Year','FontSize', 14)
+ylabel('NDVI','FontSize', 14)
+set(gca, 'FontSize', 14);
+text(10, 0.95, '(b) Double crop', 'FontSize', 18);
